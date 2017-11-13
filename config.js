f6280c87250e1f2a7bad9651298a0831b17a5767
Madhav Maheshwar <mahesh@Madhavs-MacBook-Pro.local>
2017-11-13 19:58:03 +0530

    Env update 1

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 var env = process.env.NODE_ENV || 'development';
 // console.log('env ****', env);
 
-if(env === 'production' || env === 'development' || env === 'test') {
+if(env === 'production') {
+    process.env.MONGODB_URI = 'mongodb://db_todo_app:todoapp@ds249355.mlab.com:49355/todoapp';
+    process.env.JWT_SECRET = 'Cappuccino';
+} else if(env === 'development' || env === 'test') {
     var config = require('./config.json');
     var envConfig = config[env];
 
